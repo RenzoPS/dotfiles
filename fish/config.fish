79a409b60e040cd70713522b4e2d58488230042d
RenzoPS <renzopiris2006@gmail.com>
2025-05-21 23:02:12 -0300

    Update kitty themes / omf / starship

--- fish/config.fish
+++ fish/config.fish
@@ -1,7 +1,26 @@
-if status is-interactive
-    # Commands to run in interactive sessions can go here
-end
+set -g fish_color_normal white
+set -g fish_color_command brcyan
+set -g fish_color_keyword magenta
+set -g fish_color_quote yellow
+set -g fish_color_redirection brblue
+set -g fish_color_end brmagenta
+set -g fish_color_error brred
+set -g fish_color_param white
+set -g fish_color_comment brblack
+set -g fish_color_operator brcyan
+set -g fish_color_escape brgreen
+set -g fish_color_autosuggestion 5e5c64
+set -g fish_color_selection white --bold --background=brblack
+set -g fish_color_search_match --background=brmagenta
+set -g fish_color_user green
+set -g fish_color_host cyan
+set -g fish_color_cwd brmagenta
+set -g fish_color_cwd_root brred
+set -g fish_color_valid_path --underline
+set -g fish_color_match --background=brblue
 
-if test -z "$DISPLAY"; and test (tty) = /dev/tty
+if test -z "$DISPLAY"; and test (tty) = /dev/tty1
     exec Hyprland
 end
+
+starship init fish | source
